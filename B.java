d5ba316d772823df3bbf3205e70d6d5a6eafe420
abhijeet karmakar <abhijeetkarmakar87@outlook.com>
2024-06-05 07:48:31 +0530

    Update B.java

--- B.java
+++ B.java
@@ -1,9 +1,9 @@
 package myPack;
 import static java.lang.System.out;
 import java.io.*;
-class B {  // non subclass of My_pack package.
+public class B {  // non subclass of My_pack package.
 	int b = 50;
-	void disp2(){
-		out.println("a = "+a+"m = "+m+" s = "+s+" t = "+t+" b = "+b);
+	public void disp2(){
+		out.println("a = "+ a +"m = "+m+" s = "+s+" t = "+t+" b = "+b);
 	}
 }
